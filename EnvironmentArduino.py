c73456fa76a8bc353fc2b8d40ea3f9159b9de61d
labpi@hhg.lab <labpi@hhg.lab>
2021-06-21 09:34:37 +0200

    working version

--- EnvironmentArduino.py
+++ EnvironmentArduino.py
@@ -3,27 +3,23 @@
 from tango import AttrWriteType, DevState, DebugIt, DispLevel
 from tango.server import Device, attribute, command, device_property
 import serial
+from time import sleep
 
 class EnvironmentArduino(Device):
     Port = device_property(dtype=str, default_value='/dev/ttyArduino')
     Baudrate = device_property(dtype=int, default_value=115200)
 
-# ------ Attributes ------ #
-
     humidity = attribute(label='Humidity',
                          dtype=float,
                          access=AttrWriteType.READ,
                          unit='%',
-                         format='6.2f')
+                         format='6.1f')
 
-    # optionally use fget/fset to point to read and write functions.
-    # Default is "read_temperature"/"write_temperature".
-    # Added some optional attribute properties.
     temperature = attribute(label='Temperature',
                             dtype=float,
-                            access=AttrWriteType.READ_WRITE,
+                            access=AttrWriteType.READ,
                             unit='C',
-                            format="6.2f",)
+                            format="6.1f",)
 
     def init_device(self):
         Device.init_device(self)
@@ -31,7 +27,7 @@ class EnvironmentArduino(Device):
 
         try:
             self.info_stream('Connection to port {:s} with baudrate {:d}'.format(self.Port, self.Baudrate))
-            self.serial = serial.Serial(port=self.Port, baudrate=self.Baudrate)
+            self.serial = serial.Serial(port=self.Port, baudrate=self.Baudrate, timeout=0)
             if self.serial.is_open:
                 self.serial.close()
             self.serial.open()
@@ -47,11 +43,20 @@ class EnvironmentArduino(Device):
         self.set_state(DevState.OFF)
 
     def read_humidity(self):
-        return 1
+        return float(self.query('h'))
 
     def read_temperature(self):
-        return 2
+        return float(self.query('t'))
 
+    def query(self, cmd):
+        cmd_str = '{:s}\r\n'.format(cmd)
+        self.debug_stream('Sending command: {:s}'.format(cmd_str))
+        self.serial.write(cmd_str.encode('utf-8'))
+        self.serial.flush()
+        sleep(1)
+        res = self.serial.readline().decode('utf-8')
+        self.debug_stream('Receiving result: {:s}'.format(res))
+        return res
 
 # start the server
 if __name__ == "__main__":
